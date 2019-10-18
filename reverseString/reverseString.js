40bc06671725d634d92ca8b6d69c98ed3e19f252
Learn Code <vanessa.stumme@softwareone.com>
2019-10-18 13:32:15 +0200

    solved task reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let array = [];
+    let i = 1;
+    while(i <= word.length){
+        array.push(word.substr(word.length-i, 1));
+        i++;
+    }
+    return array.join('');
 }
 
 module.exports = reverseString
