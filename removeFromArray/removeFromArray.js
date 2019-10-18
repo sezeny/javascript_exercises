0547300867bbffcaffb9ffe655c58ce81528fa15
Learn Code <vanessa.stumme@softwareone.com>
2019-10-18 13:21:57 +0200

    Solved up to removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...arg) {
 
+    var args = Array.from(arg);
+    args = [...arg];
+
+    array = array.filter(value => -1 === args.indexOf(value));
+
+    return array;
 }
 
 module.exports = removeFromArray
